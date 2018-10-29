aa382143e14bad678924fa57c09bc24580b89cf7
timMacneil <tnmacnei@ncsu.edu>
2018-10-29 04:10:32 -0400

    Add files via upload

---
+++ PokemonData/server.R
@@ -0,0 +1,53 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+# Reads in pokemon data and converts NA values to "None".  Only 'Type 2' has NA values because the Pokemon
+# is only one type.  This translates that appropriately for the data.
+pokeData = read_csv("pokemon.csv")
+pokeData[is.na(pokeData)] = "None"
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+  
+  # Download button for the table display - not finished
+  output$dlData = downloadHandler(
+    filename = function() {
+      paste("Pokemon List.csv")
+    },
+    content = function(file) {
+      write.csv(pokeData, file)
+    }
+  )
+  
+  # table for pokemon data
+  output$list <- renderTable({   
+    
+    #data has not been adjusted
+    if(!input$modified){          
+      pokeData
+    }
+    
+    #data has been adjusted to meet specifications given by user
+    else{
+      pokeData[pokeData$'Type 1'== input$type1
+               & pokeData$'Type 2'== input$type2
+               & pokeData$HP>input$hp
+               & pokeData$Attack>input$att
+               & pokeData$Defense>input$def
+               & pokeData$Speed>input$spd
+               & pokeData$'Sp. Atk'>input$spAtt
+               & pokeData$'Sp. Def'>input$spDef,]
+    }
+    
+  })
+})
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Project-2
+Repository for ST555 project 2
+Data obtained from: https://www.kaggle.com/terminus7/pokemon-challenge
+This data set has records off the base statistics of all pokemon
